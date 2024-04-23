492b3219fb5bf97b5d768126982dcdea37b9cfcd
Amine Ersu <160289484+ersuamine@users.noreply.github.com>
2024-04-23 17:38:07 -0400

    Updated SwapNumbers with solution and comments

--- src/java_coding_interview_Qs/SwapNumbers.java
+++ src/java_coding_interview_Qs/SwapNumbers.java
@@ -1,4 +1,28 @@
 package java_coding_interview_Qs;
 
 public class SwapNumbers {
+    public static void main(String[] args) {
+        // make int variables
+        int x =5;
+        int y = 7;
+        System.out.println("x = " + x);
+        System.out.println("y = " + y);
+        System.out.println("Swapping values");
+
+        // add value of x and y and set it as x
+        x = x + y; //12
+
+        // swap value of y by substracting it from total value (x)
+        y = x - y; // 5
+
+        //swap value of x substracting it from total value (x)
+        x = x - y; //7
+
+        System.out.println("x = " + x);
+        System.out.println("y = " + y);
+
+    }
 }
+/*
+Swap values of two given variable without creating another variable
+ */
